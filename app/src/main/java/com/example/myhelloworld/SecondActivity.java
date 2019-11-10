8f24a79e62c6bbf94704fd631b86c4195e1234b6
Mikhail Gorbachenko <michael.gorbachenko@yandex.ru>
2019-11-10 19:48:47 +0400

    Holla

--- app/src/main/java/com/example/myhelloworld/SecondActivity.java
+++ app/src/main/java/com/example/myhelloworld/SecondActivity.java
@@ -5,7 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -16,9 +19,28 @@ public class SecondActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
-        String message = getIntent().getStringExtra(MESSAGE);
+        final String message = getIntent().getStringExtra(MESSAGE);
         TextView messagePreview = findViewById(R.id.preview_text);
         messagePreview.setText(message);
+
+        final String[] hardcodedEmails = new String[] {"pavlushasoslow@yandex.ru"};
+
+        final Button emlBtn = findViewById(R.id.email_btn); //why always final?
+        emlBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("*/*");
+                intent.putExtra(Intent.EXTRA_EMAIL, hardcodedEmails);
+                intent.putExtra(Intent.EXTRA_SUBJECT, "Holla!");
+                intent.putExtra(Intent.EXTRA_TEXT, message);
+                if(intent.resolveActivity(getPackageManager())!=null){
+                    startActivity(intent);
+                } Toast.makeText(getApplicationContext(), "there is no e-mail app", 15).show();
+
+            }
+        });
+
     }
 
     public static void start(Activity activity, String message){
